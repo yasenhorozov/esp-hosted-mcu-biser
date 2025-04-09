53cf719427891cd8687168549eda278319920587
Soh Kam Yung <sohkamyung@espressif.com>
2025-04-09 13:42:39 +0800

    bugfix(fg_mempool): Allocate mempool from DMA capable memory
    - Make sure mempool memory is allocated from DMA capable memory, even
      if PSRAM is enabled

--- slave/main/mempool.c
+++ slave/main/mempool.c
@@ -33,7 +33,7 @@ struct hosted_mempool * hosted_mempool_create(void *pre_allocated_mem,
 
 	if (!pre_allocated_mem) {
 		/* no pre-allocated mem, allocate new */
-		heap = (uint8_t *)CALLOC(1, MEMPOOL_ALIGNED(OS_MEMPOOL_BYTES(
+		heap = (uint8_t *)MEM_ALLOC(MEMPOOL_ALIGNED(OS_MEMPOOL_BYTES(
 						num_blocks,block_size)));
 		if (!heap) {
 			ESP_LOGE(TAG, "mempool create failed, no mem\n");
